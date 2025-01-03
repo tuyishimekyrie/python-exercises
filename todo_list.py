0a4118cd8cbd88433b8b2fa4730cf271e5120d8a
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-03 20:55:07 +0200

    Feat: Add new file for todolist

---
+++ todo_list.py
@@ -0,0 +1,8 @@
+def get_user_input():
+    print("1. view tasks")
+    print("2. add task")
+    print("3. delete task")
+    print("4. exit")
+    
+if __name__ == "__main__":
+    get_user_input()
